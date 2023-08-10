e5e20017054f347e4b6aa00bf316e3a73c68ddcb
Julien <julien.tissidre@gmail.com>
2023-08-10 15:28:06 +0200

    finalisation des composants et pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import Card from '../components/Card';
 import Banner from '../components/Banner';
 import data from '../data/logements';
-import '../styles/Home.scss'
+import '../styles/Home.scss';
+import BannerHome from '../assets/BannerHome.png';
 
 const Home = () => {
     return (
         <div>
-            <Banner />
+            <Banner
+                bannerImg={BannerHome}
+                bannerTitle="Chez vous, partout et ailleurs"
+                page="home"
+            />
+
             <div className="logements">
-                {data.map((logement) => (
+                {data.map(logement => (
                     <Card logement={logement} key={logement.id} />
                 ))}
             </div>
         </div>
     );
-    }
+}
 
-    export default Home;
+export default Home;
